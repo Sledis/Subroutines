cf54b99b0f44e4142237b10df224ceefc7d158f0
Sledis <49693807+Sledis@users.noreply.github.com>
2019-08-28 13:08:29 +0100

    Split the function in Functions into 3 functions.

---
+++ Functions Part 2/Functions Part 2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+	cout << "1. Search" << endl;
+	cout << "2. View record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+int processSelection() {
+
+	cout << "Enter selction:" << flush;
+
+	int input;
+	cin >> input;
+
+	return input;
+}
+
+int main()
+{
+
+	showMenu();
+	int input = processSelection();
+
+
+	switch (input) {
+	case 1:
+		cout << "Searching..." << endl;
+		break;
+	case 2:
+		cout << "Viewing..." << endl;
+		break;
+	case 3:
+		cout << "Quiting..." << endl;
+		break;
+	default:
+		cout << "Invalid selcection." << endl;
+	}
+
+	return 0;
+}
